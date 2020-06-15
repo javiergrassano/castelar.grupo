18c0fc65aeac8579598779ba9748a6e26b4b381c
javier Grassano <javier.grassano@vindow.com>
2020-06-14 21:43:53 -0300

    Se agrego seguridad

--- LPPA-Arte/ArtExWeb/Models/IdentityModels.cs
+++ LPPA-Arte/ArtExWeb/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace ArtExWeb.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("ArtExDB", throwIfV1Schema: false)
         {
         }
 
