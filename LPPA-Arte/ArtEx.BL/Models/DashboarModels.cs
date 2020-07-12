c20dc3a1f7c28aeebe50bc9416e4ca83cfa5af47
Javier Grassano <javiergrassano@hotmail.com>
2020-07-12 17:34:35 -0300

    Dashboard usuario Administrador

---
+++ LPPA-Arte/ArtEx.BL/Models/DashboarModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtEx.BL.Models
+{
+    public class SalesMonthModel
+    {
+        public string period { get; set; }
+        public int quantity { get; set; }
+        public double total { get; set; }
+    }
+
+    public class StarCountModel
+    {
+        public int star { get; set; }
+        public int quantity { get; set; }
+    }
+
+}
